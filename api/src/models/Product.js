245381da72fcf2ee57659100b443b2d9929312ff
ImanolMalamud <imanolm93@gmail.com>
2023-01-03 20:58:49 -0300

    Api Boilerplate - Version 1

---
+++ api/src/models/Product.js
@@ -0,0 +1,43 @@
+/**
+ * *******************************************************************
+ * Este archivo es solamente para usar de guía. Se lo puede modificar.
+ *
+ * Se lo usa este modelo tambien en db.js para crear relaciones.
+ *
+ * (Se tomó como ejemplo el boilerplate del PI)
+ * ********************************************************************
+ */
+
+const { DataTypes } = require("sequelize")
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = sequelize => {
+  // defino el modelo
+  sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        unique: true,
+      },
+      img: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  )
+}
