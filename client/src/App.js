1cebc6d5fc67cb9f547f13ed8b667420f261f6d2
ImanolMalamud <imanolm93@gmail.com>
2023-01-03 21:12:22 -0300

    Boilerplate - Client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Route } from "react-router-dom"
+import CreatePokemon from "./components/CreatePokemon/CreatePokemon"
+import NavBar from "./components/NavBar/NavBar"
+import Detail from "./components/Detail/Detail"
+import Landing from "./components/Landing/Landing"
+import Home from "./components/Home/Home"
+import Paginated from "./components/Paginated/Paginated"
+
+function App() {
+  return <>The Gaming Farm</>
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./redux/store/index"
+import dotenv from "dotenv"
+dotenv.config()
+
+axios.defaults.baseURL = `http://localhost:3001`
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
