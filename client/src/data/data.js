3b85417b3c73973fbf829b8adfafacf8b2e613d1
jacquec <castillojacqueline.jc@gmail.com>
2023-01-04 12:17:05 -0300

    data v0

---
+++ client/src/data/data.js
@@ -0,0 +1,53 @@
+const Product = [
+    {
+        id: 1,
+        title: 'MONITOR GAMER SAMSUNG ODYSSEY 32" G3 S32AG32',
+        price: 141.545,
+        detail: 
+        'Monitor Gamer Samsung Odyssey G3 S32ag32 Lcd 32 Negro.Un monitor a tu medida. Gracias a su pantalla LCD vas a obtener gráficas con gran nitidez, colores vivos y atractivos. Este monitor de 32" te va a resultar cómodo para estudiar, trabajar o ver una película en tus tiempos de ocio. Asimismo, su resolución de 1920 x 1080 te permite disfrutar de momentos únicos gracias a una imagen de alta fidelidad. Su tiempo de respuesta de un milisegundo lo hace ideal para gamers y cinéfilos porque es capaz de mostrar imágenes en movimiento sin halos o bordes borrosos.',
+        img: 'https://cdn.shopify.com/s/files/1/0511/7499/8190/products/MonitorGamerSamsungOdyssey32_G3S32ag32@2x.jpg?v=1664998764',
+        mark: 'SAMSUNG'
+    },
+    {
+        id: 2,
+        title: 'NOTEBOOK DELL G15 5511 I7 15.6" FHD RTX 3050 WINDOWS 11 HOME',
+        price: 458.849,
+        detail: 'Potencia garantizada: la gama de procesadores Intel® i7 11800h le permite disfrutar de un rendimiento potente sin interrumpir los juegos, el streaming o los vídeos.Refrigeración constante: el diseño térmico inspirado en Alienware incorpora una doble entrada de aire en la parte superior del teclado y en la parte inferior del portátil G15. Después, el aire se expulsa a través de cuatro rejillas de ventilación ubicadas en los laterales y en la parte posterior para maximizar el flujo de aire sobre los tubos de cobre, lo que permite una refrigeración óptima y la disipación del calor. Potencia mejorada: los últimos avances en el diseño térmico confieren al portátil Dell G15 el espacio necesario para proporcionar una gran potencia y adaptarse a las cargas fluctuantes del sistema. Esto permite que los componentes se mantengan refrigerados y que las velocidades del reloj sean más altas incluso mientras se ejecutan las aplicaciones más exigentes.',
+        img: 'https://cdn.shopify.com/s/files/1/0511/7499/8190/products/NotebookDellG155511i715.6_FHDRTX3050Windows11Home@2x.jpg?v=1668458507',
+        mark: 'DELL'
+    },
+    {
+        id: 3,
+        title: 'WEBCAM GENIUS F100 CON MICRÓFONO 1080P FULL HD',
+        price: 9.461,
+        detail: ' Esta cámara tiene un "lente gran angular" con un campo de hasta 120 grados que le permite ver un ángulo más amplio; ya no será necesario amontonarse frente a la cámara. Para las videoconferencias, la WideCam F100 incluye un micrófono estéreo incorporado que le permitirá hablar con claridad. El lente de cristal brinda una mejor calidad de imagen cuando chatea. La F100 también incluye un cable de extensión USB con el que podrá aumentar la distancia hasta tres metros.',
+        img: 'https://cdn.shopify.com/s/files/1/0511/7499/8190/products/WebCamGeniusF100@2x.jpg?v=1643903075',
+        mark: 'GENIUS',
+    },
+    {
+        id: 4,
+        title: 'AURICULAR TRUST ZAMAK GXT 414 CON MICRÓFONO',
+        price: 19.006,
+        detail: 'Auriculares multiplataforma premium para gaming. Diseño resistente con piezas de metal y un acabado de alta calidad. Suaves y cómodas almohadillas circumaurales de gran tamaño. Potentes unidades de altavoces activos de 53 mm. Micrófono extraíble y flexible y diadema ajustable. Solo hay que conectar los auriculares en el controlador de juegos para usarlos en la consola. Cable trenzado de 1 m (para consola) cable de extensión de 1 m (para PC) y un cable trenzado adicional de 1 m con micrófono integrado (para móviles).',
+        img: 'https://cdn.shopify.com/s/files/1/0511/7499/8190/products/Gxt414@2x.jpg?v=1644411195',
+        mark: 'TRUST',
+    },
+    {
+        id: 5,
+        title: 'DISCO SSD XPG GAMMIX S50 LITE 1TB M2',
+        price: 32.098,
+        detail: 'Aproveche PCIe Gen4x4, la iteración más reciente de la interfaz PCIe, con la unidad de estado sólido XPG GAMMIX S50 Lite PCIe Gen4x4 M.2 2280. Con velocidades de lectura/escritura de hasta 3900/3200 MB por segundo y con cantidad de otras funciones excelentes, la unidad GAMMIX S50 Lite llevará su sistema al siguiente nivel.',
+        img: 'https://cdn.shopify.com/s/files/1/0511/7499/8190/products/DiscoSSDXpgGammixS50Lite1TBM2@2x.jpg?v=1646232013', 
+        mark: 'ADATA',
+    },
+    {
+        id: 6,
+        title: 'FUENTE ATX XPG CORE REACTOR 750W 80 PLUS GOLD',
+        price: 38.442,
+        detail: 'XPG CORE REACTOR es una fuente de alimentación modular 80 Plus Gold equipada con condensadores 100% japoneses. Combinada con la topología resonante Line Level Control (LLC), es eficientemente energética, fiable y permite realizar el montaje de una forma ordenada y sencilla.',
+        img: 'https://cdn.shopify.com/s/files/1/0511/7499/8190/products/FuenteAtxXpgCoreReactor750w80PlusGols@2x.jpg?v=1645821905',
+        mark: 'XPG',
+    }
+]
+
+export default Product;
