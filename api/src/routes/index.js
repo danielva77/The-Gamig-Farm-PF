b364ad95d39842d4199b841503d566a24d3ef90e
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-01-04 15:02:22 -0300

    Rutas Users

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,1 +1,33 @@
-console.log("index de ruta")
+const { Router } = require('express');
+const axios = require("axios");
+
+// Modelos de la base de datos ↓
+const {  } = require("../db");
+
+const router = Router()
+
+
+// Obtener todos los usuarios
+router.get("/usuarios", async (req, res) => {
+    // AXIOS.GET("")
+}) 
+
+// obtener un usuario en particular
+router.get("/usuario/name?Nombre", async (req, res) => {
+    
+}) 
+
+// cargar/crear usuario 
+router.post("/usuario/name?Nombre", async (req, res) => {
+    
+}) 
+
+// Actualizar informacion del usuario
+router.put("/usuario/name?Nombre", async (req, res) => {
+    
+}) 
+
+// Eliminar cuenta del usuario
+router.delete("/usuario/name?Nombre", async (req, res) => {
+
+}) 
