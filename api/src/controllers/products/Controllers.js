8df2fbc2d67ba9e4eb686e982e6975f1c0bd2220
Daniel <valdez.daniel77@gmail.com>
2023-01-05 11:48:23 -0300

    get,post

---
+++ api/src/controllers/products/Controllers.js
@@ -0,0 +1,61 @@
+const { Product, Category, Mark } = require('../../db');
+
+// Funcion para traer todos los juegos, incluye el modelo categoria
+const getAllProducts = async() => {
+	try {
+  let allproducts = await Product.findAll({
+		include: [{
+			model: Category,
+			attributes: ['name'],
+			through: {
+				attributes: [],
+			},
+		}]
+	});
+      return allproducts;
+  } catch (error) {
+    alert(error)
+  }
+};
+
+// Funcion para crear productos
+const createProducts = async(){
+	const { title, price, detail, img, stock, category, mark } = req.body;
+    try {
+      if (!title || !price || !detail || !img || !stock || !category || !mark) {
+        res.status(404).send("faltan parametros") 
+    }} catch (error) {
+      res.status(404).send(error)
+    }
+
+    let productCreated = await Product.create({
+      title, 
+      price,
+      detail, 
+      img: img? img : "https://gesisarg.com/sistema-gestion/res/archivos/imagen_articulo_por_defecto.jpg", 
+      stock
+  });
+
+  if (category.length) {
+    category.map(async cat => {
+        let c = await Category.findOrCreate({
+            where: { title: cat }
+        })
+        productCreated.addCategory(c[0])
+    })
+}
+if (mark.length) {
+  mark.map(async marca => {
+      let m = await Mark.findOrCreate({
+          where: { title: marca }
+      })
+      productCreated.addMark(m[0])
+  })
+}
+  res.status(200).send("Product created succesfully")
+};
+
+module.exports = {
+  getAllProducts,
+  createProducts
+};
