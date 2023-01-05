421df740693d49f6c1949d9292f9444da0a8dd75
ImanolMalamud <imanolm93@gmail.com>
2023-01-05 13:51:23 -0300

    Ruta get product by id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,33 +1,27 @@
-const { Router } = require('express');
-const axios = require("axios");
+const { Router } = require("express")
+const axios = require("axios")
 
 // Modelos de la base de datos ↓
-const {  } = require("../db");
+const {} = require("../db")
 
 const router = Router()
 
-
 // Obtener todos los usuarios
 router.get("/usuarios", async (req, res) => {
-    // AXIOS.GET("")
-}) 
+  // AXIOS.GET("")
+})
 
 // obtener un usuario en particular
-router.get("/usuario/name?Nombre", async (req, res) => {
-    
-}) 
+router.get("/usuario/name?Nombre", async (req, res) => {})
 
-// cargar/crear usuario 
-router.post("/usuario/name?Nombre", async (req, res) => {
-    
-}) 
+// cargar/crear usuario
+router.post("/usuario/name?Nombre", async (req, res) => {})
 
 // Actualizar informacion del usuario
-router.put("/usuario/name?Nombre", async (req, res) => {
-    
-}) 
+router.put("/usuario/name?Nombre", async (req, res) => {})
 
 // Eliminar cuenta del usuario
-router.delete("/usuario/name?Nombre", async (req, res) => {
+router.delete("/usuario/name?Nombre", async (req, res) => {})
 
-}) 
+// Obtener un producto en particular
+router.get("/productos/:id", async (req, res) => {})
