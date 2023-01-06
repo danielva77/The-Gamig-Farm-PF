22ed87ffae70ab8f21c7bf8258d56f6990b43797
Benjamin Bidondo <108904979+BBidondo@users.noreply.github.com>
2023-01-06 19:31:38 -0300

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import LandingPage from "./components//Landing Page/LandingPage";
 import { Route, BrowserRouter } from "react-router-dom"
 import Home from "./components/Home/Home"
 import Details from './components/Details/Details';
-
+import {LoginButton} from './components/NavBar/NavBar';
 
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
+      <LoginButton/>
       <Route exact path='/' component={LandingPage} />
       <Route exact path='/home' component={Home} />
       <Route path='/products/:id' component={Details} />
--- client/src/index.js
+++ client/src/index.js
@@ -9,13 +9,19 @@ import { Provider } from "react-redux"
 import {store} from "./redux/store"
 import dotenv from "dotenv"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Auth0Provider } from "@auth0/auth0-react"
 
 dotenv.config()
 axios.defaults.baseURL = `http://localhost:3001`
 
 ReactDOM.render(
 <Provider store={store}>
+<Auth0Provider 
+domain="thegamingfarm.us.auth0.com" 
+clientId="cPlOrIF4eQIxbk8cPW4NVxXCsOgHLrBn" 
+redirectUri={window.location.origin}>    
 <App/>
+</Auth0Provider>
 </Provider>,
 
 document.getElementById("root")
