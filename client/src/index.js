830006ac5713f0c9521befe02a65be7204f6fa4b
Daniel <valdez.daniel77@gmail.com>
2023-01-06 01:39:20 -0300

    productos en cards

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import axios from "axios"
 // import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 import { Provider } from "react-redux"
-import store from "./redux/store"
+import {store} from "./redux/store"
 import dotenv from "dotenv"
+import App from "../src/App"
 dotenv.config()
 axios.defaults.baseURL = `http://localhost:3001`
- import App from "../src/App"
+
 // import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
