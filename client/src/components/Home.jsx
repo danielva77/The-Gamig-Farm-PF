8b858545da424c75bf52038c8fb2e4772a7affe8
MauricioSuescun <mautorres15@hotmail.com>
2023-01-05 22:24:53 -0500

    updates ultimos

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,16 @@
 import StoreItems from "./StoreItems"
 import Filter  from "./Filter"
 
+console.log("hola")
+
+
 export function Home() {
   return (
     <>
       <h1>Home</h1>
       <Filter />
       <StoreItems />
+      
     </>
   )
 }
