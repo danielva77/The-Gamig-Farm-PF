faef5f68153798591bab47488cf48d0a379d9c05
Benjamin Bidondo <108904979+BBidondo@users.noreply.github.com>
2023-01-07 12:56:08 -0300

    LogIn

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,32 @@ import { Route, BrowserRouter, Switch } from "react-router-dom"
 import Home from "./components/Home/Home"
 import Details from './components/Details/Details';
 import NavBar from './components/NavBar/NavBar';
-import Login from "./components/Login/Login"
+import { LoginButton } from "./components/Login/Login";
+import { LogoutButton } from "../src/components/Logout/Logout"
+import { Profile } from "./components/Profile/profile"
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
+
   return (
     <BrowserRouter>
     <div className="App">
+    {isAuthenticated ? (
+          <>
+      <Profile/>
+      <LogoutButton/>
+      </>
+        ) : (
+      <LoginButton/>
+      )}
       <NavBar/>
       <Switch>
       <Route exact path='/' component={LandingPage} />
       <Route path='/home' component={Home} />
       <Route path='/products/:id' component={Details} />
-      <Route path='/login' component={Login} />
       </Switch>
       <Footer/>
     </div>
--- client/src/components/Logout/Logout.jsx
+++ client/src/components/Logout/Logout.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+export const LogoutButton = () => {
+  const { logout } = useAuth0();
 
-export const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return (  
-  
-    <button onClick={() => loginWithRedirect()}  class="cssbuttons-io">
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}  class="cssbuttons-io">
   <span><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 0h24v24H0z" fill="none"></path>
     <path d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z" fill="currentColor"></path>
-    </svg> Log In</span>
+    </svg> Log Out</span>
   </button> 
-
-)
+  );
 };
-
