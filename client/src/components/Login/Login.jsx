171894e1461245cf319549107081ddc8b521d750
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-01-07 14:20:59 -0300

    working in JSON

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -6,38 +6,47 @@ import { useDispatch, useSelector } from 'react-redux'
 export default function ActivityCreate() {
   return (
     <div className='created'>
-      <Link to={"/"}> <button>Volver</button></Link>
+      <Link to={"/"}><button type="button" class="btn btn-outline-danger mt-3">Volver</button></Link>
 
       <h1>Crea tu Perfil</h1>
 
-      <form >
+      <form className='bg-success'>
 
-        <div>
-          <h3>Nombre y Apellido</h3>
-          <input type="text" name="name" required />
+
+        {/* NAME AND LASTNAME */}
+        <div class="input-group">
+          <span class="input-group-text bg-warning">Nombre y Apellido</span>
+          <input type="text" aria-label="First name" className="form-control bg-black bg-gradient text-light" placeholder='Nombre...' />
+          <input type="text" aria-label="Last name" class="form-control bg-black bg-gradient text-light" placeholder='Apellido...' />
         </div>
 
-        <div>
-          <h4 >Cargar Foto</h4>
-          <label><input type="file"/></label>
+        <div class="input-group mt-2">
+          <label class="input-group-text bg-warning" for="inputGroupFile01">Avatar</label>
+          <input type="file" class="form-control bg-black text-light" id="inputGroupFile01" />
         </div>
 
-        <div>
-          <h4 >Correo Electronico:</h4>
-          <input type="text" name="duration" required className='creacion' />
+        <div class="input-group mt-2">
+          <span class="input-group-text bg-warning" id="basic-addon1">@ Correo Electronico</span>
+          <input type="text" class="form-control bg-black text-light" placeholder="example@henry.com" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
 
-        <div>
-          <h4>password</h4>
-          <label><input type="password" required/></label>
+        <div class="input-group mt-2">
+          <span class="input-group-text bg-warning" id="basic-addon1">Password</span>
+          <input type="password" class="form-control bg-black text-light" placeholder="Example:  xF34rRA_1" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
 
-        <button type='submit' className='creacion1'>Registrarse</button>
-        
+        <button type='submit' className='btn btn-primary p-4 m-4'>Registrarse</button>
 
-        
+
+
       </form>
 
+
+      <div>
+
+
+
+      </div>
 
     </div>
   )
