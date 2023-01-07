24866ce27b6fd3e1a857e6007bee2f063b37e2be
BradleyGCF <bradleycarucci@gmail.com>
2023-01-07 14:34:45 -0400

    cambios

--- api/src/controllers/products/Controllers.js
+++ api/src/controllers/products/Controllers.js
@@ -4,17 +4,44 @@ const { Product, Mark, Category } = require("../../db");
 const getAllProducts = async (req, res, next) => {
 
   try {
-      const allproducts = await Product.findAll({
-          include: [
-            {
-              model: Category,
-            },
-            {
-              model: Mark,
-            }
-        ]})
+      const productos = 
+      [
+        {
+            "id": "a14338b0-304c-4da3-a61c-7ddd97e72d4f",
+            "title": "Mouse",
+            "price": 2,
+            "detail": "mouse inalambrico pro",
+            "img": "https://http2.mlstatic.com/D_NQ_NP_610024-MLA44840092138_022021-O.webp",
+            "stock": 3,
+            "createdAt": "2023-01-06T12:34:23.088Z",
+            "updatedAt": "2023-01-06T12:34:23.088Z",
+            "MarkId": null,
+            "Categories": [
+                {
+                    "id": "06953941-7114-46ee-bc63-262e89f79919",
+                    "title": "perifericos",
+                    "Product_Category": {
+                        "createdAt": "2023-01-06T12:34:23.137Z",
+                        "updatedAt": "2023-01-06T12:34:23.137Z",
+                        "ProductId": "a14338b0-304c-4da3-a61c-7ddd97e72d4f",
+                        "CategoryId": "06953941-7114-46ee-bc63-262e89f79919"
+                    }
+                }
+            ],
+            "Mark": null
+        }
+    ]
+      // const allproducts = await Product.findAll({
+      //     include: [
+      //       {
+      //         model: Category,
+      //       },
+      //       {
+      //         model: Mark,
+      //       }
+      //   ]})
         
-        res.status(200).send(allproducts)
+        res.status(200).json(productos)
   } catch (error) {
     res.status(404).send("aun no hay nada")
   }
