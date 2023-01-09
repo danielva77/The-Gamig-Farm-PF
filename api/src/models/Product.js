fc87b0f92be4d1d0877909852dac3b6a88a9d810
ImanolMalamud <imanolm93@gmail.com>
2023-01-08 22:06:40 -0300

    Agregue info a data.json. En Product hice que la img pueda ser null

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,37 +1,35 @@
-
 const { DataTypes } = require("sequelize")
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = sequelize => {
   // defino el modelo
-  sequelize.define("Product",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      price: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
-      detail: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      img: {
-        type: DataTypes.STRING,
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      }
+  sequelize.define("Product", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
     },
-  )
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    detail: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  })
 }
