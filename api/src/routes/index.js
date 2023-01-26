9e6915e4fb7760f10e2aab2c063dc8f5486b1c23
Benjamin Bidondo <108904979+BBidondo@users.noreply.github.com>
2023-01-26 12:20:58 -0300

    act

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -361,6 +361,9 @@ const consola = [
   { title: "Nintendo DS" },
   { title: "Wii" },
   { title: "Nintendo Switch" },
+  { title: "Nintendo 3DS" },
+  { title: "Nintendo (NES)" },
+  { title: "PSP" },
 ];
 
 const mark = [{ title: "Juegos" }, { title: "Mandos" }];
@@ -427,7 +430,7 @@ const filterByMandos = async () => {
     where: { title: "Mandos" },
     include: {
       model: Product,
-      attributes: ["id", "title", "price", "detail", "img", "stock", "isActive"],
+      attributes: ["id", "title", "price", "detail", "img", "stock", "mark", "isActive"],
       through: {
         attributes: [],
       },
@@ -441,7 +444,7 @@ const filterByJuegos = async () => {
     where: { title: "Juegos" },
     include: {
       model: Product,
-      attributes: ["id", "title", "price", "detail", "img", "stock", "isActive"],
+      attributes: ["id", "title", "price", "detail", "img", "stock","mark", "isActive"],
       through: {
         attributes: [],
       },
@@ -456,7 +459,7 @@ const filterByCategory = async (consola) => {
     where: { title: consola },
     include: {
       model: Product,
-      attributes: ["id", "title", "price", "detail", "img", "stock"],
+      attributes: ["id", "title", "price", "detail", "img", "stock","mark" ],
       through: {
         attributes: [],
       },
