41a014cbeb9d1bb5ff07672b2359ee788329e594
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-03-13 18:30:24 -0300

    asd

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -5,6 +5,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./News.css";
 
+
+
 export default function News() {
   const [news, setNews] = useState([]);
   useEffect(() => {
