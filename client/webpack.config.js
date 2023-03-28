67ac01755d302c8ea237d1bdc1d535abbf15c096
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-03-28 00:40:09 -0300

    changes finale

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  // ... tu configuración de Webpack ...
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
-    // Agrega el plugin de dotenv-webpack
-    new Dotenv()
-  ]
-}
+    new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+  },
+};
