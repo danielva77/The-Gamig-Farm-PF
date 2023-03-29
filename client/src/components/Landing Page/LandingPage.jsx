1b8ff51d0a5999e5cd4997ebaa0f234803643b47
Benjamin Bidondo <108904979+BBidondo@users.noreply.github.com>
2023-03-29 09:38:53 -0300

    Landing ruta

--- client/src/components/Landing Page/LandingPage.jsx
+++ client/src/components/Landing Page/LandingPage.jsx
@@ -27,8 +27,7 @@ const LandingPage = () => {
           <h1 className="tituloLanding">Bienvenid@s a TheGamingFarm! </h1>
 
           {/* <img  className="fuego" src={Fuego}  /> */}
-          <NavLink to="/products" onClick=
-            {("/home")}>
+          <NavLink to="/products" onClick={() => { window.location.href = "/home"; }}>
             <button class="cssbuttons-io">
               <span className="letrasss">
                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
